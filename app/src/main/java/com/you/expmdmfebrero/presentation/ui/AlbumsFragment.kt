418661de1f40a54d3bdd9f48a740d2497b94e70d
alvaro <alvaromunozvaquero@gmail.com>
2025-02-14 19:39:45 +0100

    FENESH 2

--- app/src/main/java/com/you/expmdmfebrero/presentation/ui/AlbumsFragment.kt
+++ app/src/main/java/com/you/expmdmfebrero/presentation/ui/AlbumsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.you.expmdmfebrero.databinding.FragmentAlbumsBinding
 import com.you.expmdmfebrero.presentation.adapter.album.AlbumAdapter
@@ -68,6 +69,10 @@ class AlbumsFragment : Fragment() {
 
         }
 
+    }
+
+    private fun navigateToSetas(){
+        findNavController().navigate(AlbumFragmentDirectoins().albumListToSetasList())
     }
 
     override fun onDestroyView() {
